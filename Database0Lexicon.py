6ef96b2fd1b0552ae8edec99706f4725bab7e224
thedabs91 <33946919+thedabs91@users.noreply.github.com>
2020-08-30 09:46:22 -0600

    Update Database0Lexicon.py
    Made more things functions, to make things easier.

--- Database0Lexicon.py
+++ Database0Lexicon.py
@@ -31,9 +31,10 @@ def create_table(conn, create_table_sql):
 
 
 # I will not worry about Collins, for now.
-def create_entry(conn, entryrow):
+def create_entry(conn, entryrow, lexicon = 'twl'):
     #Creating a new gram
-    sql = ''' INSERT INTO lexicon_twl(gram, word, length, score, vowels, threeplus, jqxz, remfirst, remlast, fhook, bhook)
+    sql = 'INSERT INTO lexicon_' + lexicon +\
+          '''(gram, word, length, score, vowels, threeplus, jqxz, remfirst, remlast, fhook, bhook)
               VALUES(?,?,?,?,?,?,?,?,?,?,?) '''
     cur = conn.cursor()
     cur.execute(sql, entryrow)
@@ -50,54 +51,34 @@ def alphasort(string):
         output = output + ltr
     return(output)
 
+# Opening the database
 database = "/home/david/Documents/Common Stuff/PythonPrograms/Scrabble/Users/database.db"
 conn = db_connector(database)
 
-# First step: creating the table for the lexicon
-# The name is `lexicon_twl` because the word list used was the TWL lexicon
-# ... well actually the NWL, but I prefer the old name of TWL.
-# Similar code can also be used for other lexica
+# First step: creating the table
 
-lex_twl_table = """ CREATE TABLE IF NOT EXISTS lexicon_twl(
-                        gram text NOT NULL,
-                        word text PRIMARY KEY,
-                        length integer,
-                        score integer,
-                        vowels integer,
-                        threeplus integer,
-                        jqxz integer,
-                        remfirst integer,
-                        remlast integer,
-                        fhook text,
-                        bhook text
-                    );"""
+def lex_table(lexicon):
+    lexicon_table = 'CREATE TABLE IF NOT EXISTS lexicon_' + lexicon +\
+                    	'''(gram text NOT NULL,
+                            word text PRIMARY KEY,
+                            length integer,
+                            score integer,
+                            vowels integer,
+                            threeplus integer,
+                            jqxz integer,
+                            remfirst integer,
+                            remlast integer,
+                            fhook text,
+                            bhook text
+                            );'''
+    
+    if conn is not None:
+        create_table(conn, lexicon_table)
 
-if conn is not None:
-    create_table(conn, lex_twl_table)
 
-# Now I will show the csw_table. Nothing will be done for now,
-# ... but it will return later.
-
-lex_csw_table = """ CREATE TABLE IF NOT EXISTS lex_csw_twl(
-                        gram text NOT NULL,
-                        word text PRIMARY KEY,
-                        length integer,
-                        score integer,
-                        vowels integer,
-                        threeplus integer,
-                        jqxz integer,
-                        remfirst integer,
-                        remlast integer,
-                        fhook text,
-                        bhook text
-                    );"""
-
-if conn is not None:
-    create_table(conn, lex_csw_table)
 
 
 # Second Step: adding the words
-# For now, this will only be for the TWL table.
 
 def multicount(string, ltrs):
     output = 0
@@ -105,23 +86,33 @@ def multicount(string, ltrs):
         output += string.count(ltr)
     return (output)
 
-file = open('Words/UpdatedDic', 'r')
-for line in file:
-    line = line.rstrip('\n')
-    line = line.split(',')
-    length = len(line[0])
-    nv = line[0].count('A') + line[0].count('E') + line[0].count('I') + \
-         line[0].count('O') + line[0].count('U')
-    njqxz = line[0].count('J') + line[0].count('Q') + line[0].count('X') + line[0].count('Z')
-    ntp = multicount(line[0], 'BCFHKMPVWY') + njqxz
-    pts = nv + multicount(line[0], 'LNRST') + 2*multicount(line[0], 'DG') +\
-          3*multicount(line[0], 'BCMP') + 4*multicount(line[0], 'FHVWY') + 5*line[0].count('K') +\
-          8*multicount(line[0], 'JX') + 10*multicount(line[0],'QZ')
-    gram = alphasort(line[0])
-    with conn:
-        entry = (gram, line[1], length, pts, nv, ntp, njqxz, int(line[4]=='y'), int(line[5]=='y'), line[2], line[3])
-        create_entry(conn, entry)
 
-file.close()
+def lex_db(lexicon):
+    file = open('Words/UpdatedDic_' + lexicon, 'r')
+    x = 1
+    for line in file:
+        line = line.rstrip('\n')
+        line = line.split(',')
+        length = len(line[0])
+        nv = line[0].count('A') + line[0].count('E') + line[0].count('I') + \
+            line[0].count('O') + line[0].count('U')
+        njqxz = line[0].count('J') + line[0].count('Q') + line[0].count('X') + line[0].count('Z')
+        ntp = multicount(line[0], 'BCFHKMPVWY') + njqxz
+        pts = nv + multicount(line[0], 'LNRST') + 2*multicount(line[0], 'DG') +\
+              3*multicount(line[0], 'BCMP') + 4*multicount(line[0], 'FHVWY') + 5*line[0].count('K') +\
+              8*multicount(line[0], 'JX') + 10*multicount(line[0],'QZ')
+        gram = alphasort(line[0])
+        with conn:
+            entry = (gram, line[1], length, pts, nv, ntp, njqxz, int(line[4]=='y'), int(line[5]=='y'), line[2], line[3])
+            create_entry(conn, entry, lexicon = lexicon)
+        if x%10000 == 0:
+            print(x)
+        x += 1
+    print(x)
+    file.close()
+
+# This seems to have worked!
+
+## To use the function, I recommend specifying the word list that you want in the file.
 
 # This seems to have worked!
